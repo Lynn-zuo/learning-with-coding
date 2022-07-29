1d87a1002e303e43d4babac3517a44d7b6c5c559
zuolin <zuolin@wedoctor.com>
2022-07-29 20:23:01 +0800

    feat: react组件生命周期

--- React/react-npm/src/index.js
+++ React/react-npm/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './component-lifecycle/App.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
