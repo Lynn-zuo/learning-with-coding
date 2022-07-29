2855184e01c34138ec4304cc4c333cf35e81abe4
zuolin <zuolin@wedoctor.com>
2022-07-29 14:34:56 +0800

    feat: 类组件定义

--- React/react-npm/src/App.js
+++ React/react-npm/src/App.js
@@ -1,9 +1,31 @@
-import { Component } from 'react';
-
-class App extends Component {
-    render() {
-        return <h2>hello world</h2>
+import { Component } from "react"
+export default class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+        count: 0
     }
-}
+  }
+  render() {
+    return (
+      <div>
+        <h2>类组件定义</h2>
+        <p>1. 组件名称必须以大写开头，小写视为原生标签</p>
+        <p>2. 类组件必须继承自react.Component</p>
+        <p>3. render()函数是唯一必须实现的方法</p>
+        <p>4. constructor初始化组数据，可选实现</p>
+        <p>5. super()方法基于类继承，在constructor中调用</p>
+        <p>6. this.state维护的是组件内部数据</p>
+        <p>{ this.state.count }</p>
+        <button onClick={(e) => this.increment(e)}>+</button>
+      </div>
+    )
+  }
 
-export { App }
+  increment(e) {
+    console.log(e, '++++')
+    this.setState({
+        count: this.state.count + 1
+    })
+  }
+}
