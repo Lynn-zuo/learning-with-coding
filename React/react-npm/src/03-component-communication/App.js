0047c486b46982a58d093ef7d066dd7ae01eeef0
zuolin <zuolin@wedoctor.com>
2022-07-30 10:25:26 +0800

    feat: 父子组件间通信父传子props

--- React/react-npm/src/03-component-communication/App.js
+++ React/react-npm/src/03-component-communication/App.js
@@ -1,34 +1,71 @@
 import { Component } from "react"
+import PropTypes from 'prop-types'
+
+class ComponentA extends Component { // 类组件获取父组件传递的参数
+  constructor(props) {
+    super(props) // 父类保存 - 类的继承原理ES5
+    // this.props=props
+    this.state = {}
+  }
+  static propTypes = { // 方式二：es6中的class field写法
+    fatherName: PropTypes.string,
+    count: PropTypes.number,
+    films: PropTypes.array
+  }
+  render() {
+    // 可以通过react-test-render拿到props
+    console.log('ComponentA--执行了render方法')
+    const { fatherName, count, films } = this.props
+    return (
+      <div>
+        <h2>{fatherName}的子组件</h2>
+        <p>{fatherName}父组件传递的参数{count}</p>
+        <ul>
+          {films.map(film => <li key={film}>{film}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+ComponentA.propTypes = { // 方式一
+  fatherName: PropTypes.string,
+  count: PropTypes.number,
+  films: PropTypes.array
+}
+ComponentA.defaultProps = {
+  fatherName: 'PropTypes.string',
+  count: 'PropTypes.number',
+  films: 'PropTypes.array'
+}
+
+// function ComponentA (props) { // 函数组件获取父组件传递的参数
+//   const { fatherName, count } = props
+//   return (
+//     <div>
+//       <h2>{fatherName}的子组件</h2>
+//       <p>{fatherName}父组件传递的参数{count}</p>
+//     </div>
+//   )
+// }
+
 export default class App extends Component {
   constructor(props) {
     super()
     this.state = {
       count: 0,
-      isShow: false,
+      films: ['大话西游', '月光宝盒', '西游记', '三国演义']
     }
   }
-  componentDidMount() {
-    console.log("APP-mount生命周期---首次渲染")
-  }
-  componentDidUpdate() {
-    console.log("APP-update生命周期---更新渲染")
-  }
-  componentWillUnmount() {
-    console.log("APP-unmount生命周期--移除DOM")
-  }
   render() {
-    const { count, isShow } = this.state
-    console.log('APP--执行了render方法')
+    const { count, films } = this.state
     return (
       <div>
         <h2>组件生命周期</h2>
         <p>{count}</p>
         <button onClick={(e) => this.increment(e)}>+</button>
         <hr />
-        <button onClick={(e) => this.changeShow(e)}>change</button>
-        {isShow && <ComponentA />}
-        {/* v-show只更新APP */}
-        <ComponentA style={{ display: isShow ? "block" : "none" }} />
+        <ComponentA fatherName='App' films={films} count={count} />
       </div>
     )
   }
@@ -37,28 +74,5 @@ export default class App extends Component {
     this.setState({
       count: this.state.count + 1,
     })
-  }
-  changeShow(e) {
-    this.setState({
-      isShow: !this.state.isShow,
-    })
-  }
-}
-
-class ComponentA extends Component {
-  componentDidMount() {
-    console.log("ComponentA-mount生命周期---首次渲染")
-  }
-  componentWillUnmount() {
-    console.log("ComponentA-unmount生命周期--移除DOM")
-  }
-  render() {
-    console.log('ComponentA--执行了render方法')
-    return (
-      <div>
-        <h2>App的字组件</h2>
-        <p>测试卸载生命周期</p>
-      </div>
-    )
   }
 }
--- React/react-npm/src/index.js
+++ React/react-npm/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './02-component-lifecycle/App.js'
+import App from './03-component-communication/App.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
