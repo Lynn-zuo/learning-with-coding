ae004915184c95c2c17479e5bd7ec7e11b77901c
zuolin <zuolin@wedoctor.com>
2022-08-04 23:35:11 +0800

    feat: 切回React18版本

--- React/react-npm/src/index.js
+++ React/react-npm/src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './06-setState/App.js'
 import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
