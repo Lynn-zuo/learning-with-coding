1f48523c1d13831c27b59461329b48412a71778e
zuolin <zuolin@wedoctor.com>
2022-08-17 09:39:28 +0800

    feat: 使用redux-devtools插件追踪store变化

--- React/react-redux/src/store/index.js
+++ React/react-redux/src/store/index.js
@@ -1,11 +1,14 @@
-import {legacy_createStore as createStore, applyMiddleware } from 'redux'
+import {legacy_createStore as createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import reducer from './reducer.js'
+
+// 添加redux-devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose;
 
 // 应用一些中间件
 const storeEnhancer = applyMiddleware(thunkMiddleware)
 
 // store -- 创建的时候需要传入一个reducer纯函数
-const store = createStore(reducer, storeEnhancer)
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(storeEnhancer))
 
 export default store
