7c83c43aa439daff867ea8377b19fceb5cc172d4
zuolin <zuolin@wedoctor.com>
2022-08-22 21:19:28 +0800

    feat: useCallback结合memo实现性能优化

--- React/react-hooks/src/views/09-useCallback/index.js
+++ React/react-hooks/src/views/09-useCallback/index.js
@@ -1,24 +1,32 @@
-import React, {useState, useCallback} from 'react'
+import React, {useState, useCallback, memo} from 'react'
 
-export default function Count() {
+const LCButton = memo((props) => { // 执行浅层比较
+    console.log(props.buttonTxt,'重新渲染了')
+    return <button onClick={props.action}>{props.buttonTxt}</button>
+})
+
+export default function UseCallback() {
+  console.log('useCallback重新渲染')
+  const [show, setShow] = useState(false)
   const [count, setCount] = useState(0)
-  function increment() {
+  function increment() { // 始终重新渲染
     console.log('执行了increment')
     setCount(count + 1)
   }
-  const subtract = useCallback(() => { // 这种写法不存在性能优化
-    console.log('执行了decrement')
+  const subtract = useCallback(() => { // 结合memo实现性能优化
+    console.log('执行了decrement') // 这种写法单独不存在性能优化
     // setCount(prev => prev - 1) 
     setCount(count - 1) // 如果不在第二个参数数组中传入count
   }, [count])// 就会一直引用初始count的值，不会重新渲染
 
   return (
     <div>
-        <h2>Counter</h2>
+        <h2>UseCallback</h2>
         <div>
             <p>当前计数：{count}</p>
-            <button onClick={increment}>+</button>
-            <button onClick={subtract}>-</button>
+            <LCButton buttonTxt='++' action={increment} />
+            <LCButton buttonTxt='--' action={subtract} />
+            <button onClick={() => setShow(!show)}>show切换</button>
         </div>
     </div>
   )
