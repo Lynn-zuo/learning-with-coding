c3d449f9de1a9f52c72b3728f152592b1afcfb79
zuolin <zuolin@wedoctor.com>
2022-08-22 09:31:01 +0800

    feat: useState源码解读理解入参,函数合并

--- React/react-hooks/src/App.js
+++ React/react-hooks/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-// import Count from '@/views/01-Count'
+import Count from '@/views/01-Count'
 // import MultiState from '@/views/02-MultiState'
-import ComplexStateModify from '@/views/03-ComplexStateModify'
+// import ComplexStateModify from '@/views/03-ComplexStateModify'
 function App() {
   return (
     <div className="App">
@@ -9,9 +9,9 @@ function App() {
         APP
       </header>
       <main>
-        {/* <Count /> */}
+        <Count />
         {/* <MultiState /> */}
-        <ComplexStateModify />
+        {/* <ComplexStateModify /> */}
       </main>
     </div>
   );
--- React/react-hooks/src/views/01-Count/index.js
+++ React/react-hooks/src/views/01-Count/index.js
@@ -2,13 +2,23 @@ import React, {useState} from 'react'
 
 export default function Count() {
   const [count, setCount] = useState(0)
+  function increment() { // 会合并，最终只+1
+    setCount(count + 1)
+    setCount(count + 1)
+    setCount(count + 1)
+  }
+  function subtract() { // 不会合并，+3
+    setCount(prev => prev - 1)
+    setCount(prev => prev - 1)
+    setCount(prev => prev - 1)
+  }
   return (
     <div>
         <h2>Counter</h2>
         <div>
             <p>当前计数：{count}</p>
-            <button onClick={() => {setCount(count + 1)}}>+</button>
-            <button onClick={() => {setCount(count - 1)}}>-</button>
+            <button onClick={increment}>+</button>
+            <button onClick={subtract}>-</button>
         </div>
     </div>
   )
